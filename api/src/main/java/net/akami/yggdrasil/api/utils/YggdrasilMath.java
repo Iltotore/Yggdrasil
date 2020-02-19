fdabc093398186eb31ec41c4910b0093ff615f4a
Iltotore <rafbodaha@gmail.com>
2020-02-19 14:03:38 +0100

    Added YggdrasilMath#getTargetEntity
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- api/src/main/java/net/akami/yggdrasil/api/utils/YggdrasilMath.java
+++ api/src/main/java/net/akami/yggdrasil/api/utils/YggdrasilMath.java
@@ -1,9 +1,18 @@
 package net.akami.yggdrasil.api.utils;
 
 import com.flowpowered.math.vector.Vector3d;
+import net.akami.yggdrasil.api.life.LivingEntity;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.util.AABB;
+import org.spongepowered.api.util.Tuple;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
+import java.util.Optional;
+import java.util.TreeMap;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class YggdrasilMath {
 
@@ -27,10 +36,26 @@ public class YggdrasilMath {
     }
 
     public static Function<Integer, Float> standardPolynomialFunction(float initial) {
-        return (tier) -> initial + (float) Math.pow(2*tier - 1, 1.3) - 1;
+        return (tier) -> initial + (float) Math.pow(2 * tier - 1, 1.3) - 1;
     }
 
     public static BiFunction<Float, Integer, Float> instantStandardPolynomialFunction(float initial) {
         return instantCostFunction(standardPolynomialFunction(initial));
+    }
+
+    public static Entity getTargetEntity(Location<World> location, Vector3d direction, double range, Predicate<Entity> filter) {
+        TreeMap<Double, Entity> treeMap = new TreeMap<>(Double::compareTo);
+        for(Entity entity : location.getExtent().getNearbyEntities(location.getPosition(), range)) {
+            if(!filter.test(entity)) continue;
+            if(!(entity instanceof LivingEntity)) continue;
+            if(!entity.getBoundingBox().isPresent()) continue;
+            AABB boundingBox = entity.getBoundingBox().get();
+            Vector3d vectorLocation = location.getPosition();
+            Optional<Tuple<Vector3d, Vector3d>> intersection = boundingBox.intersects(vectorLocation, direction);
+            if(!intersection.isPresent()) continue;
+            treeMap.put(intersection.get().getFirst().distance(vectorLocation), entity);
+        }
+        if(treeMap.isEmpty()) return null;
+        return treeMap.lastEntry().getValue();
     }
 }
